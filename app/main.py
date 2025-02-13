e562b1edb8a88262932d07f1343d4c3c666b281a
vlados <2006krickunow@gmail.com>
2025-02-13 17:04:14 +0300

    add pages router to FastAPI application

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from app.order.router import router as order_router
+from app.pages.router import router as page_router
 
 app = FastAPI()
 app.include_router(order_router)
+app.include_router(page_router)
